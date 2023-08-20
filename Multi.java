d2bec3767d00b1a0f1ef485f2338bfb081fdad56
Harshal Patel <hp842484n@gmail.com>
2023-08-20 20:34:50 +0530

    added Multi.java

---
+++ Multi.java
@@ -0,0 +1,10 @@
+public interface Multi {
+    public static void main(String[] args) {
+
+        // delcaring varibales
+        int var1 = 55;
+        int var2 = 15;
+        int product = var1 * var2;
+        System.out.println("Product of " + var1 + " and " + var2 + " is: " + product);
+    }
+}

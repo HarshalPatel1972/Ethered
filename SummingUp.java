4f2bad4fd97f59252db0f1770bb16492b785af48
Harshal Patel <hp842484n@gmail.com>
2023-08-20 19:51:27 +0530

    initial commit

---
+++ SummingUp.java
@@ -0,0 +1,10 @@
+public class SummingUp {
+    public static void main(String[] args) {
+
+        // delcaring varibales
+        int var1 = 5;
+        int var2 = 15;
+        int sum = var1 + var2;
+        System.out.println("Sum of " + var1 + " and " + var2 + " is: " + sum);
+    }
+}

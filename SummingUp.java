ec723b0932728fe2d3cc7a674fb6384b6c107f02
Harshal Patel <hp842484n@gmail.com>
2023-08-20 20:23:29 +0530

    added message in Summing.jaba

--- SummingUp.java
+++ SummingUp.java
@@ -2,7 +2,7 @@ public class SummingUp {
     public static void main(String[] args) {
 
         // delcaring varibales
-        int var1 = 5;
+        int var1 = 55;
         int var2 = 15;
         int sum = var1 + var2;
         System.out.println("Sum of " + var1 + " and " + var2 + " is: " + sum);

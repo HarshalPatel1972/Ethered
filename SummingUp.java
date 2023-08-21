de989f95055697fe0ae4205fb190da1088abff04
Harshal Patel <106813066+HarshalPatel1972@users.noreply.github.com>
2023-08-21 18:46:58 +0530

    Update SummingUp.java

--- SummingUp.java
+++ SummingUp.java
@@ -2,8 +2,8 @@ public class SummingUp {
     public static void main(String[] args) {
 
         // delcaring varibales
-        int var1 = 55;
-        int var2 = 15;
+        int var1 = 78;
+        int var2 = 95;
         int sum = var1 + var2;
         System.out.println("Sum of " + var1 + " and " + var2 + " is: " + sum);
     }
